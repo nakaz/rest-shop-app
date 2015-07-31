348c729d8a16f9cd07beabb60a271887e289c99e
Sean Nakamura <seanenakamura@gmail.com>
2015-07-30 21:18:11 -1000

    submit new order POSTs and appends to DOM

--- public/js/app.js
+++ public/js/app.js
@@ -15,6 +15,20 @@ $(function (){
     });
   });
 
+  $('#product').submit(function(evt){
+    evt.preventDefault(); //i'll take care of the rest
+    var id = $('#product_num').val();
+    $.ajax({
+      url: serverURL + '/products/' + id
+    }).done(function(product){
+      $('#info').html('');
+      $('#info').append('<div class="box"></div>')
+                  .append('<div class="name">'+product.name+'</div>')
+                  .append('<div class="desc">'+product.description+'</div>')
+                  .append('<div class="price">'+product.price+'</div>');
+    });
+  });
+
   $('.orders').click(function(evt){
     $.ajax({
       url: serverURL + '/orders'
@@ -28,4 +42,42 @@ $(function (){
       });
     });
   });
+
+  $('#order').submit(function(evt){
+    evt.preventDefault(); //i'll take care of the rest
+    var id = $('#order_num').val();
+    $.ajax({
+      url: serverURL + '/orders/' + id
+    }).done(function(order){
+      $('#info').html('');
+      $('#info').append('<div class="box"></div>')
+                  .append('<div class="name">'+order.name+'</div>')
+                  .append('<div class="desc">'+order.quantity+'</div>')
+                  .append('<div class="price">'+order.product_id+'</div>');
+    });
+  });
+
+  $('#newOrder').submit(function(evt){
+    evt.preventDefault();
+    var name = $('#name').val();
+    var quantity = $('#quantity').val();
+    var id = $('#product_id').val();
+    var newOrder = {name: name, quantity: quantity, product_id: id};
+    $.ajax({
+      url: serverURL + '/orders',
+      type: 'POST',
+      data: JSON.stringify(newOrder),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      async: false,
+      success: function (order){
+        $('#info').html('');
+        $('#info').append('<div class="box"></div>')
+                    .append('<div class="name">'+order.name+'</div>')
+                    .append('<div class="desc">'+order.quantity+'</div>')
+                    .append('<div class="price">'+order.product_id+'</div>');
+      }
+    });
+  });
+
 });
