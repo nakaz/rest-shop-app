9d3772e2f903c3be8aece71808a366261bb664b5
Sean Nakamura <seanenakamura@gmail.com>
2015-08-01 15:42:09 -1000

    prototype css colors

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,26 @@ $(function (){
   function productBox (element){
     return '<div class="name">'+element.name+'</div>' +
            '<div class="desc">'+element.description+'</div>' +
-           '<div class="price">'+element.price+'</div>';
+           '<div class="price">$'+element.price+'</div>';
   }
 
   function orderBox (element){
+    var quantity = Number(element.quantity);
+    var product = Number(element.Product.price);
+    var total = quantity * product;
     return '<div class="name">'+element.name+'</div>' +
-           '<div class="quantity">'+element.quantity+'</div>' +
            '<div class="productName">'+element.Product.name+'</div>' +
-           '<div class="productPrice">'+element.Product.price+'</div>';
+           '<div class="quantity">'+element.quantity+' x ( $'+element.Product.price+' )</div>' +
+           '<div class="price">Total: $'+total+'</div>';
+  }
+
+  function receipt (element){
+    var quantity = Number(element.quantity);
+    var product = Number(element.Product.price);
+    var total = quantity * product;
+    return '<h1>Thank you for your purchase!</h1>' +
+           orderBox(element) +
+           '<div class="totalprice">Total: $'+total+'</div>';
   }
 
   // ajax for list counts
@@ -41,9 +53,9 @@ $(function (){
     $.ajax({
       url: serverURL + '/products'
     }).done(function (products){
-      $('#container').html('');
+      $('#grid').html('');
       products.forEach(function (element, index){
-        $('#container').append('<div class="box">' + productBox(element) + '</div>');
+        $('#grid').append('<li class="box">' + productBox(element) + '</li>');
       });
     });
   });
@@ -56,8 +68,8 @@ $(function (){
     $.ajax({
       url: serverURL + '/products/' + id
     }).done(function(product){
-      $('#container').html('');
-      $('#container').append('<div class="box">' + productBox(product) + '</div>');
+      $('#grid').html('');
+      $('#grid').append('<div class="box">' + productBox(product) + '</div>');
     });
   });
 
@@ -65,9 +77,9 @@ $(function (){
     $.ajax({
       url: serverURL + '/orders'
     }).done(function (orders){
-      $('#container').html('');
+      $('#grid').html('');
       orders.forEach(function (element, index){
-        $('#container').append('<div class="box">' + orderBox(element) + '</div>');
+        $('#grid').append('<li class="box">' + orderBox(element) + '</li>');
       });
     });
   });
@@ -78,8 +90,8 @@ $(function (){
     $.ajax({
       url: serverURL + '/orders/' + id
     }).done(function(order){
-      $('#container').html('');
-      $('#container').append('<div class="box">' + orderBox(order) + '</div>');
+      $('#grid').html('');
+      $('#grid').append('<div class="box">' + orderBox(order) + '</div>');
     });
   });
 
@@ -87,11 +99,15 @@ $(function (){
     $('#myModal').foundation('reveal','open');
   });
 
+  var quantity = null;
+  var product = null;
+
   $('#newOrder').submit(function(evt){
     evt.preventDefault();
     var name = $('#name').val();
     var quantity = $('#quantity').val();
     var id = $('#order-product_id').val();
+
     var newOrder = {name: name, quantity: quantity, product_id: id};
     $.ajax({
       url: serverURL + '/orders',
@@ -102,7 +118,7 @@ $(function (){
       async: false,
       success: function (order){
         $('.reveal-modal').html('');
-        $('.reveal-modal').append('<div class="box">' + orderBox(order) + '</div>');
+        $('.reveal-modal').append('<div class="box">' + receipt(order) + '</div>');
         $('#order_id').append($('<option>').val(order.id).html(order.name));
       }
     });
